1bb6695e95649dd6665b034d11ae81dd28ed6b72
boris_t <boris@talovikov.ru>
2014-10-08 14:01:28 +0600

    code review; code documentation;

--- init.php
+++ init.php
@@ -1,46 +1,74 @@
 <?php
+/**
+* $Id$
+*
+* @category Init
+* @package  Dotlan_Mod
+* @author   Khi3l, boris_t <boris@talovikov.ru>
+* @license  http://opensource.org/licenses/MIT MIT
+*/
 
-$modInfo['dotlan_mod']['name'] = 'Dotlan Map';
-$modInfo['dotlan_mod']['abstract'] = 'Provide menu with links to dotlan maps.';
-$modInfo['dotlan_mod']['about'] = 'by Greetings, Khi3l, for <a href="http://www.babylonknights.com">Babylon Knights</a>';
+$modInfo['dotlan_mod'] = array(
+    'name' => 'Dotlan Map',
+    'abstract' => 'Provide menu with links to dotlan maps.',
+    'about'=> 'by Greetings, Khi3l, for <a href="http://www.babylonknights.com">Babylon Knights</a>',
+);
 
-event::register('killDetail_context_assembling', 'dotlan::add');
+event::register('killDetail_context_assembling', 'Dotlan::add');
 
-class dotlan {
+/**
+ * Provides callback for event::register.
+ */
+class Dotlan
+{
 
-  function add($page) {
-    $page->addBehind('menu', 'dotlan::show');
-  }
-  
-  function show($page) {
-
-    global $smarty;
-
-    if($page->kll_id) {
-      $kill = new Kill($page->kll_id);
+    /**
+     * Adds a callbacks in the queue.
+     *
+     * @param pKillDetail $page object of pKillDetail class
+     *
+     * @return none
+     */
+    static function add($page)
+    {
+        $page->addBehind('menu', 'Dotlan::show');
     }
-    else {
-      $kill = new Kill($page->kll_external_id, TRUE);
-    }
 
-    $system = $kill->getSystem();
-    $system_id = $system->getID();
-    $constellation_id = $system->getConstellationID();
-    $region_id = $system->getRegionID();
+    /**
+     * Render "Dotlan Maps" menu.
+     *
+     * @param pKillDetail $page object of pKillDetail class
+     *
+     * @return none
+     */
+    static function show($page)
+    {
+        global $smarty;
 
-    $menubox = new box('Dotlan Maps');
-    $menubox->setIcon('menu-item.gif');
-    $dotlan = 'http://evemaps.dotlan.net';
-    if($system_id) {
-      $menubox->addOption('link', 'Show me on map', "${dotlan}/map/${system_id}");
-      $menubox->addOption('link', 'System', "${dotlan}/system/${system_id}");
+        if ($page->kll_id) {
+            $kill = new Kill($page->kll_id);
+        } else {
+            $kill = new Kill($page->kll_external_id, true);
+        }
+
+        $system = $kill->getSystem();
+        $system_id = $system->getID();
+        $constellation_id = $system->getConstellationID();
+        $region_id = $system->getRegionID();
+
+        $menubox = new box('Dotlan Maps');
+        $menubox->setIcon('menu-item.gif');
+        $dotlan = 'http://evemaps.dotlan.net';
+        if ($system_id) {
+            $menubox->addOption('link', 'Show me on map', "${dotlan}/map/${system_id}");
+            $menubox->addOption('link', 'System', "${dotlan}/system/${system_id}");
+        }
+        if ($constellation_id) {
+            $menubox->addOption('link', 'Constellation', "${dotlan}/map/${constellation_id}");
+        }
+        if ($region_id) {
+            $menubox->addOption('link', 'Region', "${dotlan}/map/${region_id}");
+        }
+        return $menubox->generate();
     }
-    if($constellation_id) {
-      $menubox->addOption('link', 'Constellation', "${dotlan}/map/${constellation_id}");
-    }
-    if($region_id) {
-      $menubox->addOption('link', 'Region', "${dotlan}/map/${region_id}");
-    }
-    return $menubox->generate();
-  }
 }
