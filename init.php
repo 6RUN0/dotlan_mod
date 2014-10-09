04a390a2521a4933578646bd1ef7fd22269d6296
botis_t <boris@talovikov.ru>
2014-10-09 22:56:51 +0600

    add dotlan menu on system_detail page

--- init.php
+++ init.php
@@ -1,12 +1,12 @@
 <?php
 /**
-* $Id$
-*
-* @category Init
-* @package  Dotlan_Mod
-* @author   Khi3l, boris_t <boris@talovikov.ru>
-* @license  http://opensource.org/licenses/MIT MIT
-*/
+ * $Id$
+ *
+ * @category Init
+ * @package  Dotlan_Mod
+ * @author   Khi3l, boris_t <boris@talovikov.ru>
+ * @license  http://opensource.org/licenses/MIT MIT
+ */
 
 $modInfo['dotlan_mod'] = array(
     'name' => 'Dotlan Map',
@@ -14,14 +14,14 @@ $modInfo['dotlan_mod'] = array(
     'about'=> 'by Greetings, Khi3l, for <a href="http://www.babylonknights.com">Babylon Knights</a>',
 );
 
-event::register('killDetail_context_assembling', 'Dotlan::add');
+event::register('killDetail_context_assembling', 'Dotlan::killDetailEvent');
+event::register('systemdetail_context_assembling', 'Dotlan::systemDetailEvent');
 
 /**
  * Provides callback for event::register.
  */
 class Dotlan
 {
-
     /**
      * Adds a callbacks in the queue.
      *
@@ -29,29 +29,65 @@ class Dotlan
      *
      * @return none
      */
-    static function add($page)
+    static function killDetailEvent($page)
     {
-        $page->addBehind('menu', 'Dotlan::show');
+        $page->addBehind('menu', 'Dotlan::killDetail');
     }
 
     /**
-     * Render "Dotlan Maps" menu.
+     * Render "Dotlan Maps" menu on kill_detail page.
      *
      * @param pKillDetail $page object of pKillDetail class
      *
-     * @return none
+     * @return string html
      */
-    static function show($page)
+    static function killDetail($page)
     {
-        global $smarty;
-
         if ($page->kll_id) {
             $kill = new Kill($page->kll_id);
         } else {
             $kill = new Kill($page->kll_external_id, true);
         }
-
         $system = $kill->getSystem();
+        return self::menu($system);
+    }
+
+    /**
+     * Adds a callbacks in the queue.
+     *
+     * @param pSystemDetail $page object of pSystemDetail class
+     *
+     * @return none
+     */
+    static function systemDetailEvent($page)
+    {
+        $page->addBehind('menu', 'Dotlan::systemDetail');
+    }
+
+    /**
+     * Render "Dotlan Maps" menu on system_detail page.
+     *
+     * @param pSystemDetail $page object of pSystemDetail class
+     *
+     * @return string html
+     */
+    static function systemDetail($page)
+    {
+        $system = new SolarSystem($page->sys_id);
+        return self::menu($system);
+    }
+
+    /**
+     * Render menu.
+     *
+     * @param int|string $system        system id
+     * @param int|string $constellation constellation id
+     * @param int|string $region        region id
+     *
+     * @return string html
+     */
+    static function menu($system)
+    {
         $system_id = $system->getID();
         $constellation_id = $system->getConstellationID();
         $region_id = $system->getRegionID();
